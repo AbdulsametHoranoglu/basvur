b7da80f067d6a572f5414e8cad905c65e1a66ef2
samethoranoglu <115842440+samethoranoglu@users.noreply.github.com>
2022-12-14 17:42:04 +0300

    Create basvurualım.cs

---
+++ basvurualım.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace _202503024__Mercan_Esen_
+{
+    public partial class basvurualım : Form
+    {
+        public basvurualım()
+        {
+            InitializeComponent();
+        }
+     
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+ 
+            formkurs fr3 = new formkurs();
+            fr3.ShowDialog();
+        }
+        
+
+    }
+}
